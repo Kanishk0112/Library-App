3fd9f18e6dd75a59c0257f9fcfce2189085e3bfd
Kanishk0112 <64630243+Kanishk0112@users.noreply.github.com>
2021-02-01 21:06:23 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import{ createAppContainer,createSwitchNavigator} from "react-navigation"
+import{createBottomTabNavigator} from "react-navigation-tabs"
+import Transactionscreen from "./Screens/transaction"
+import Searchscreen from "./Screens/search"
+export default function App() {
+  return (
+  <AppContainer/>
+    
+  );
+}
+const TabNavigator= createBottomTabNavigator({
+  Transactionscreen:{screen:Transactionscreen},
+  Searchscreen:{screen:Searchscreen}
+})
+const AppContainer= createAppContainer(TabNavigator)
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
